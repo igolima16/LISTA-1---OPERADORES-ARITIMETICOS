ed3741b7700c4353e6ebf8a705065b04ea2cbabe
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-07-01 19:58:10 -0300

    Add files via upload

---
+++ exercicio01/exercicio01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1 = -1;
+
+            
+            while (p1 < 0)
+            {
+                Console.Write("Digite a nota da P1 (valor positivo): ");
+                p1 = Convert.ToDouble(Console.ReadLine());
+
+                if (p1 < 0)
+                {
+                    Console.WriteLine("Nota inválida. Por favor, digite um valor positivo.");
+                }
+            }
+
+            
+            double mediaAprovacao = 5.0;
+
+            
+            double p2Necessaria = (3 * mediaAprovacao - p1) / 2;
+
+            
+            Console.WriteLine($"Para ser aprovado, você precisa tirar pelo menos {p2Necessaria:F2} na P2.");
+        }
+    }
+}
