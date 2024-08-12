e8a33a384f7606aa73a7b4a8d0975a4903e0e361
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-08-12 16:23:32 -0300

    Add files via upload

---
+++ OPA-ex11/OPA-ex11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPA_ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorx, valory, resultado;
+
+            Console.WriteLine("Digite o valor da base");
+            valorx = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do expoente");
+            valory = double.Parse(Console.ReadLine());
+
+            resultado = Math.Pow(valorx, valory);
+
+            Console.WriteLine("O valor da base elevada ao expoente é " + resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
