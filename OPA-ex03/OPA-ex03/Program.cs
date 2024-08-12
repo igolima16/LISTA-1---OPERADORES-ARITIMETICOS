d2d95838ecc76510c00f8af2a8848176a572d328
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-08-12 16:17:20 -0300

    Add files via upload

---
+++ OPA-ex03/OPA-ex03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPA_ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diagonal, lado, area;
+
+            Console.WriteLine("Digite o valor da diagonal do quadrado: ");
+            diagonal = double.Parse(Console.ReadLine());
+
+            lado = diagonal/Math.Sqrt(2);
+
+            area = Math.Pow(lado, 2);
+
+            Console.WriteLine("O valor da sua área é: " + area);
+
+
+            Console.ReadKey();
+        }
+    }
+}
