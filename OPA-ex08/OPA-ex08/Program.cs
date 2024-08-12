dded54fe03e6c63b54a494408dd5d56f2595319c
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-08-12 16:21:20 -0300

    Add files via upload

---
+++ OPA-ex08/OPA-ex08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPA_ex08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, fahrenheit;
+
+            Console.WriteLine("Digite a temperatura a ser convertida de celsius para fahrenheit: ");
+            celsius = double.Parse(Console.ReadLine());
+
+            fahrenheit = (celsius * 1.8) + 32;
+
+            Console.WriteLine("O valor convertido de celsius para fahrenheit é de; " + fahrenheit);
+
+            Console.ReadKey();
+        }
+    }
+}
