ea3e61bb7f26af87bf3c44aa03419b9b51d8cf05
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-08-12 16:22:13 -0300

    Add files via upload

---
+++ OPA-ex09/OPA-ex09/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPA_ex09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diametro, raio, area;
+
+            Console.WriteLine("Digite o diametro do circulo: ");
+            diametro = double.Parse(Console.ReadLine());
+
+            raio = diametro / 2;
+
+            area = Math.PI * Math.Pow(raio,2);
+
+            Console.WriteLine("O valor da area é de: " + Math.Round(area,2));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
