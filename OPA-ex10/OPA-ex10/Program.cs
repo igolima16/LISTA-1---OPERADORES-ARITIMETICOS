ec713c68d0a677c1bbcc61caa884cca1533850b7
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-08-12 16:22:56 -0300

    Add files via upload

---
+++ OPA-ex10/OPA-ex10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPA_ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cotaçãodolar, valordolar, valorreal;
+
+            Console.WriteLine("Digite o valor da cotação do dolar: ");
+            cotaçãodolar = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor que deseja converter de dolar para real: ");
+            valordolar = double.Parse(Console.ReadLine());
+
+            valorreal = valordolar * cotaçãodolar;
+
+            Console.WriteLine("$" + valordolar +" " + "Dolares é igual a R$" + valorreal);
+
+            Console.ReadKey();
+        }
+    }
+}
