2759032ffd41d2108296e2a58f0b29d08454f0af
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-08-12 16:20:08 -0300

    Add files via upload

---
+++ OPA-ex06/OPA-ex06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPA_ex06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, resultado;
+
+            Console.WriteLine("Digite o 1º numero: ");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2º numero: ");
+            num2 = double.Parse(Console.ReadLine());        
+
+            resultado = Math.Sqrt(num1 + num2 );
+
+            Console.WriteLine("O valor da média geométrica é: " + Math.Round(resultado,2));
+
+            Console.ReadKey();
+
+        }
+    }
+}
