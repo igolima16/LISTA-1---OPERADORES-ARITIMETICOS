7855f5152af47f75956cac57b62a9089c9069314
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-08-12 16:20:49 -0300

    Add files via upload

---
+++ OPA-ex07/OPA-ex07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPA_ex07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhasmaritimas, resultado;
+
+            Console.WriteLine("Digite a quantidade de milhas marítimas para converter para quilometros: ");
+            milhasmaritimas = double.Parse(Console.ReadLine());
+
+            resultado = milhasmaritimas * 1852;
+
+            Console.WriteLine("A quantidade de milhas maritimas é: " + resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
