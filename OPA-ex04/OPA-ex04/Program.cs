657bc4259fc28bcb8dea7dad68b36b244647b810
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-08-12 16:19:05 -0300

    Add files via upload

---
+++ OPA-ex04/OPA-ex04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPA_ex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int basedotriangulo, alturadotriangulo;
+            double area;
+
+            Console.WriteLine("Digite o valor da base do triangulo: ");
+            basedotriangulo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da altura do triangulo: ");
+            alturadotriangulo = int.Parse(Console.ReadLine());
+
+            area = (alturadotriangulo * basedotriangulo);
+
+            Console.WriteLine("A area do triangulo é: " +  area);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
